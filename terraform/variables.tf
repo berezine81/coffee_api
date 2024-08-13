35992449ce3ba50a12572947b4813c6d57c0d5c8
Eugene <berezine81@gmail.com>
2024-08-13 17:44:55 +0300

    Test

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "repo_name" {
+  description = "The repository name"
+  type = string
+  default = "coffee_api"
+}
+
+variable "branch_default_name" {
+  description = "The branch name"
+  type = string
+  default = "env/staging"
+}
+
+variable "db_password" {
+  description = "The database password"
+  type = string
+}
+
+variable "db_user" {
+  description = "The database user"
+  type = string
+  default = "dbuser"
+}
+
+variable "token" {
+  description = "The repo token"
+  type = string
+}
